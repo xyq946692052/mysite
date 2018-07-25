5bf524cfd9ef64a3bc4354da5c861fc9a3f5d885
xyq946692052 <xyq2312@sina.com>
2018-07-26 00:29:53 +0800

    add the count of blogs type and date

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.blog_list, name='blog_list'),
     path('blog_detail/<int:blog_pk>', views.blog_detail, name='blog_detail'),
     path('blog_type/<int:blog_type_pk>', views.blogs_with_type, name='blogs_with_type'),
+    path('date/<int:year>/<int:month>',views.blogs_with_date, name='blogs_with_date'),
 ]
