d7508ae0893096f8fb972b3f7ee2f3995ec99a39
Kevin Xie <kevin.xie@r-pac.com.cn>
2018-08-06 17:24:52 +0800

    fix bug

--- mysite/forms.py
+++ mysite/forms.py
@@ -12,7 +12,7 @@ class LoginForm(forms.Form):
         username = self.cleaned_data['username']
         password = self.cleaned_data['password']
 
-        user = auth.authenticate(username=username, password=[password])
+        user = auth.authenticate(username=username, password=password)
         if user is  None:
             raise forms.ValidationError('用户名或密码不正确')
         else:
