400ca68166067557348b214e26f0efdf876ae876
Kevin Xie <kevin.xie@r-pac.com.cn>
2018-08-20 12:21:28 +0800

    if login can like

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.db.models import Count
 from read_statistics.utils import read_statistics_once_read
 from .models import Blog, BlogType
+from mysite.forms import LoginForm
 
 
 # Create your views here.
@@ -75,6 +76,7 @@ def blog_detail(request, blog_pk):
 
     context = dict()
     context['blog'] = blog
+    context['login_form'] = LoginForm()
     context['previous_blog'] = Blog.objects.filter(created_time__gt=blog.created_time).last() #上一篇
     context['next_blog'] = Blog.objects.filter(created_time__lt=blog.created_time).first() #下一篇
     response = render(request,'blog/blog_detail.html', context)  #相应
